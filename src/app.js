3bf9c2b7a2846350b745bc706454a1b8a3f34f0b
FerRom1979 <fernando_0135@hotmail.com>
2021-03-21 17:34:53 -0300

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ const forecast = require("./utils/forecast");
 
 const app = express();
 
+// Port to use Heroku
+const port = process.env.PORT || 3000;
+
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
 const viewsPath = path.join(__dirname, "../templates/views");
@@ -91,6 +94,6 @@ app.get("*", (req, res) => {
     name: "Fernando",
   });
 });
-app.listen(3000, () => {
-  console.log(chalk.yellow("Server on port 3000"));
+app.listen(port, () => {
+  console.log(chalk.yellow(`Server on port ${port}`));
 });
